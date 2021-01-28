8f43b65aff9680e423ea58dd0af3b1cb3c002706
d2gin <672636920@qq.com>
2021-01-28 22:09:07 +0800

    更新示例

---
+++ demo/runme.php
@@ -0,0 +1,13 @@
+<?php
+
+use video2gif\GifStickers;
+
+$handle = new GifStickers([
+    'ffmpeg.binaries'  => 'D:\ffmpeg\bin\ffmpeg.exe',
+    'ffprobe.binaries' => 'D:\ffmpeg\bin\ffprobe.exe',
+]);
+if ($handle->generate('template.mp4', 'template.ass', '1.gif')) {
+    echo "finished";
+} else {
+    var_dump($handle->getError());
+}
